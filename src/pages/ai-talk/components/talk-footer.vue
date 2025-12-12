6e24e2aa49c822089b4fa54bc5aae5903f471be3
whbbit1999 <luckyboy_dong@sina.com>
2025-12-12 11:17:09 +0800

    feat: enhance talk footer with dropdown menu and improved input group layout

--- src/pages/ai-talk/components/talk-footer.vue
+++ src/pages/ai-talk/components/talk-footer.vue
@@ -1,5 +1,9 @@
 <script lang="ts" setup>
-import { ArrowUp, Paperclip } from 'lucide-vue-next'
+import { ArrowUpIcon, Paperclip } from 'lucide-vue-next'
+
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
+import { InputGroup, InputGroupAddon, InputGroupButton, InputGroupText, InputGroupTextarea } from '@/components/ui/input-group'
+import { Separator } from '@/components/ui/separator'
 
 import TalkType from './talk-type.vue'
 
@@ -17,22 +21,52 @@ function handleSubmit() {
 </script>
 
 <template>
-  <div class="flex items-center justify-center">
-    <main class="w-full p-2 border rounded-md">
-      <UiTextarea v-model="text" placeholder="input something" class="mb-2 border-none resize-none outline-1" />
+  <InputGroup>
+    <InputGroupTextarea v-model="text" placeholder="Ask, Search or Chat..." />
+    <InputGroupAddon align="block-end">
+      <TalkType @update:type="handleTypeChange" />
 
-      <div class="flex items-center">
-        <TalkType @update:type="handleTypeChange" />
+      <InputGroupButton
+        variant="ghost"
+        class="rounded-full"
+        size="icon-xs"
+      >
+        <Paperclip class="size-4" />
+        <span class="sr-only">Add File</span>
+      </InputGroupButton>
 
-        <div class="flex-1" />
+      <DropdownMenu>
+        <DropdownMenuTrigger as-child>
+          <InputGroupButton variant="ghost">
+            Auto
+          </InputGroupButton>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent
+          side="top"
+          align="start"
+          class="[--radius:0.95rem]"
+        >
+          <DropdownMenuItem>Auto</DropdownMenuItem>
+          <DropdownMenuItem>Agent</DropdownMenuItem>
+          <DropdownMenuItem>Manual</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
 
-        <UiButton variant="ghost">
-          <Paperclip class="size-4" />
-        </UiButton>
-        <UiButton variant="outline" size="icon" class="rounded-full" :disabled="!text" @click="handleSubmit">
-          <ArrowUp class="size-4" />
-        </UiButton>
-      </div>
-    </main>
-  </div>
+      <InputGroupText class="ml-auto">
+        52% used
+      </InputGroupText>
+      <Separator orientation="vertical" class="h-4!" />
+
+      <InputGroupButton
+        variant="default"
+        class="rounded-full"
+        size="icon-xs"
+        :disabled="!text"
+        @click="handleSubmit"
+      >
+        <ArrowUpIcon class="size-4" />
+        <span class="sr-only">Add File</span>
+      </InputGroupButton>
+    </InputGroupAddon>
+  </InputGroup>
 </template>
--- src/pages/ai-talk/components/talk-type.vue
+++ src/pages/ai-talk/components/talk-type.vue
@@ -16,7 +16,7 @@ watch(type, () => {
 </script>
 
 <template>
-  <UiToggleGroup v-model="type" type="single" variant="outline">
+  <UiToggleGroup v-model="type" type="single" variant="outline" size="sm">
     <UiToggleGroupItem v-for="modelType in types" :key="modelType.type" class="px-4" :value="modelType.type" :aria-label="modelType.type">
       <component :is="modelType.icon" class="size-4 mr-1" />
       {{ modelType.type }}
